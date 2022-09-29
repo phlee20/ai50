e72e9731a390ceece49fa9ad77d5c12dc451f847
Philip Lee <philiplee@Phils-M1-Air.local>
2022-09-29 13:43:31 -0400

    Implemented Sentence functions

--- projects/2020/x/minesweeper/minesweeper.py
+++ projects/2020/x/minesweeper/minesweeper.py
@@ -105,27 +105,30 @@ class Sentence():
         """
         Returns the set of all cells in self.cells known to be mines.
         """
-        raise NotImplementedError
+        if self.count == self.cells:
+            return self.cells
 
     def known_safes(self):
         """
         Returns the set of all cells in self.cells known to be safe.
         """
-        raise NotImplementedError
+        if self.count == 0:
+            return self.cells
 
     def mark_mine(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be a mine.
         """
-        raise NotImplementedError
+        self.cells.remove(cell)
+        self.count -= 1
 
     def mark_safe(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be safe.
         """
-        raise NotImplementedError
+        self.cells.remove(cell)
 
 
 class MinesweeperAI():
